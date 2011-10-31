ae498da21f9a2bf3b8e79202d77d6a9f307bc600
Mattias Liljeson <mattiasliljeson@gmail.com>
2011-10-31 18:27:41 +0100

    RaceCourse object implemented. \n\n Now contains both the image of the racecourse as well as it spedmask and checkpoints as a rectangle array

--- Common/src/common/RaceCourse.java
+++ Common/src/common/RaceCourse.java
@@ -3,25 +3,28 @@
  */
 package common;
 
+import java.awt.Rectangle;
 import java.io.Serializable;
+import javax.swing.ImageIcon;
 
 /**
- *
+ * Used as a struct, a data carrier. No sensitive data, therefore public is used
+ * on all variables.
  * @author Mattias Liljeson <mattiasliljeson.gmail.com>
  */
 public class RaceCourse implements Serializable{
-    private static RaceCourse instance = null;
+    public ImageIcon raceCourseImg;
+    public ImageIcon speedMaskImg;
+    public Rectangle[] checkpoints;
     
-    private RaceCourse(){
+    
+    public RaceCourse(ImageIcon raceCourseImg, ImageIcon speedMaskImg, Rectangle[] checkpoints){
+        this.raceCourseImg = raceCourseImg;
+        this.speedMaskImg = speedMaskImg;
+        this.checkpoints = checkpoints;
     }
     
-    public static RaceCourse getInstance(){ 
-        if(instance == null)
-            instance = new RaceCourse();
-        
-        return instance;
-    }
-
+    // Easteregg
     @Override
     public String toString() {
         String msg = "The RaceCourse object says hi!";
